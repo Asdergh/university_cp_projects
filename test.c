46a8dd592d394e9c31d1e210f093fa5fa9def5a8
Asdergh <ramzanhac2005gmail.com>
2023-03-21 13:05:32 +0300

    //header all changes were added
    //body
    
    //footer

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<time.h>
+#include<stdio.h>
+#include<locale.h>
+
+
+int main(){
+    time_t start, end;
+    double it_1, it_2;
+    print_f("введите нужные вам числа: ");
+    scan_f("%d%d", &it_1, &it_2);
+    if(it_start < it_2){
+        for(; it_1 < it_2)
+    }
+
+
+}
