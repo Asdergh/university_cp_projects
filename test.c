4f79d2dc9740cf7634d3db003dd1198e00babdef
Asdergh <ramzanhac2005gmail.com>
2023-03-21 13:29:46 +0300

    all cahnges was added

--- test.c
+++ test.c
@@ -6,11 +6,20 @@
 int main(){
     time_t start, end;
     double it_1, it_2;
-    print_f("введите нужные вам числа: ");
-    scan_f("%d%d", &it_1, &it_2);
-    if(it_start < it_2){
-        for(; it_1 < it_2)
+    printf("введите нужные вам числа: ");
+    scanf("%d%d", &it_1, &it_2);
+    if(it_1 < it_2){
+        for(; it_1 < it_2; it_1++){
+            printf("%i; \n");
+        };
+        printf("")
     }
+    else{
+        for(; it_2 < it_1; it_2 ++){
+            printf("%i; \n");
+        }
+    }
+    return 0;
 
 
 }
